7d4692d24424d788295576f198d17c7b77632869
Neosoulink <neodarksoulink@gmail.com>
2021-12-28 01:34:15 +0100

    Set project

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as THREE from "three";
+import { Canvas } from "react-three-fiber";
+
+function App() {
+	return <div className="App"></div>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// COMPONENTS
+import App from "./App";
+
+// STYLES
+import "./assets/css/index.css";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
